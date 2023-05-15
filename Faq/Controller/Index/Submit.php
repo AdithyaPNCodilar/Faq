b2c3d7fea58cdbfb9b793539ea77ec978bd8c8b8
AdithyaPNCodilar <adithya.pn@codilar.com>
2023-05-15 23:42:10 +0530

    customer_id fixed

--- Faq/Controller/Index/Submit.php
+++ Faq/Controller/Index/Submit.php
@@ -4,20 +4,24 @@ namespace Codilar\Faq\Controller\Index;
 
 use Magento\Framework\App\Action\Context;
 use Codilar\Faq\Model\FaqFactory;
+use Magento\Customer\Model\Session;
 
 class Submit extends \Magento\Framework\App\Action\Action
 {
     protected $_faqFactory;
     protected $resultRedirect;
+    protected $customerSession;
  
     public function __construct(
         Context $context, 
         FaqFactory $faqFactory, 
-        \Magento\Framework\Controller\Result\Redirect $resultRedirect
+        \Magento\Framework\Controller\Result\Redirect $resultRedirect,
+        Session $customerSession
     ) {
         parent::__construct($context);
         $this->_faqFactory = $faqFactory;
         $this->resultRedirect = $resultRedirect;
+        $this->customerSession = $customerSession;
     }
     public function execute()
     {
@@ -28,11 +32,10 @@ class Submit extends \Magento\Framework\App\Action\Action
         
         // set the product_id and customer_id
         $faq->setData('product_id', $postData['product_id']);
-        $faq->setData('customer_id', $postData['customer_id']);
+        $faq->setData('customer_id', $this->customerSession->getCustomerId());
 
         $faq->save();
         $this->messageManager->addSuccess(__('FAQ question submitted successfully!'));
         return $this->resultRedirectFactory->create()->setUrl($this->_redirect->getRefererUrl());
-    }
-  
+    }  
 }
